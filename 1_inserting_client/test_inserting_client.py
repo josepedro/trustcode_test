e16e0f08a1dfc0a0fd149514cd9a4f9c682618de
José Pedro <1jpsneto@gmail.com>
2017-08-24 18:47:52 -0300

    Inserting client done

--- 1_inserting_client/test_inserting_client.py
+++ 1_inserting_client/test_inserting_client.py
@@ -1,23 +1,18 @@
 import unittest
 import odoorpc
+import inserting_client
 
 class InsertingClientTestCase(unittest.TestCase):
 
     def test_insert_client(self):
+        inserting_client.insert_client()
         odoo = odoorpc.ODOO('chocotech.trustcode.com.br', port=80)
         db_list = odoo.db.list()
         odoo.login(db_list[5], 'demo', 'demo')
-        user = odoo.env.user
-        print(user.name)            # name of the user connected
-        print(user.company_id.name) # the name of its company
-        # Simple 'raw' query
-        user_data = odoo.execute('res.users', 'read', [user.id])
-        print(user_data)
-        print("===========================================================")
-	    # Update data through a record
-        #user.name = "Brian Jones"
-        #user = odoo.env.user
-        #print(user.name)
-
+        partner = odoo.env['res.partner']
+        assert partner.browse(1).name == 'Jose Pedro de Santana Neto', "Problem with name" 
+        assert partner.browse(1).email == '1jpsneto@gmail.com', "Problem with email"
+        assert partner.browse(1).phone == '61985909581', "Problem with phone"
+        
 if __name__ == '__main__':
 	unittest.main()
